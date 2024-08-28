b4d06fc6e52f2cd7df7f5af84a1f3660c584df78
neilha <neilharrold@gmail.com>
2024-08-28 09:54:59 +0100

    adds router api

--- lib/publishing_platform_api.rb
+++ lib/publishing_platform_api.rb
@@ -1,11 +1,25 @@
 require "addressable"
 require "publishing_platform_location"
 require "time"
-require "publishing_platform_api/publishing_api"
 require "publishing_platform_api/content_store"
+require "publishing_platform_api/publishing_api"
+require "publishing_platform_api/router"
 
 # @api documented
 module PublishingPlatformApi
+  # Creates a PublishingPlatformApi::ContentStore adapter
+  #
+  # This will set a bearer token if a CONTENT_STORE_BEARER_TOKEN environment
+  # variable is set
+  #
+  # @return [PublishingPlatformApi::ContentStore]
+  def self.content_store(options = {})
+    PublishingPlatformApi::ContentStore.new(
+      PublishingPlatformLocation.find("content-store"),
+      { bearer_token: ENV["CONTENT_STORE_BEARER_TOKEN"] }.merge(options),
+    )
+  end
+
   # Creates a PublishingPlatformApi::PublishingApi adapter
   #
   # This will set a bearer token if a PUBLISHING_API_BEARER_TOKEN environment
@@ -19,16 +33,16 @@ module PublishingPlatformApi
     )
   end
 
-  # Creates a PublishingPlatformApi::ContentStore adapter
+  # Creates a PublishingPlatformApi::Router adapter for communicating with Router API
   #
-  # This will set a bearer token if a CONTENT_STORE_BEARER_TOKEN environment
+  # This will set a bearer token if a ROUTER_API_BEARER_TOKEN environment
   # variable is set
   #
-  # @return [PublishingPlatformApi::ContentStore]
-  def self.content_store(options = {})
-    PublishingPlatformApi::ContentStore.new(
-      PublishingPlatformLocation.find("content-store"),
-      { bearer_token: ENV["CONTENT_STORE_BEARER_TOKEN"] }.merge(options),
+  # @return [PublishingPlatformApi::Router]
+  def self.router(options = {})
+    PublishingPlatformApi::Router.new(
+      PublishingPlatformLocation.find("router-api"),
+      { bearer_token: ENV["ROUTER_API_BEARER_TOKEN"] }.merge(options),
     )
   end
 end
