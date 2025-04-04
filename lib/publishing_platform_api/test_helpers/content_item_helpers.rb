2211d631f688806f6cde6db5022e38bd37712df9
neilha <neilharrold@gmail.com>
2025-04-04 10:56:53 +0100

    adds content store test helpers

---
+++ lib/publishing_platform_api/test_helpers/content_item_helpers.rb
@@ -0,0 +1,41 @@
+module PublishingPlatformApi
+  module TestHelpers
+    module ContentItemHelpers
+      def content_item_for_base_path(base_path)
+        {
+          "title" => titleize_base_path(base_path),
+          "description" => "Description for #{base_path}",
+          "schema_name" => "answer",
+          "document_type" => "answer",
+          "public_updated_at" => "2014-05-06T12:01:00+00:00",
+          # base_path is added in as necessary (ie for content-store GET responses)
+          # "base_path" => base_path,
+          "details" => {
+            "body" => "Some content for #{base_path}",
+          },
+        }
+      end
+
+      def gone_content_item_for_base_path(base_path)
+        {
+          "title" => nil,
+          "description" => nil,
+          "document_type" => "gone",
+          "schema_name" => "gone",
+          "public_updated_at" => nil,
+          "base_path" => base_path,
+          "withdrawn_notice" => {},
+          "details" => {},
+        }
+      end
+
+      def titleize_base_path(base_path, options = {})
+        if options[:title_case]
+          base_path.tr("-", " ").gsub(/\b./, &:upcase)
+        else
+          base_path.gsub(%r{[-/]}, " ").strip.capitalize
+        end
+      end
+    end
+  end
+end
---
+++ lib/publishing_platform_api/test_helpers/content_store.rb
@@ -0,0 +1,84 @@
+require "publishing_platform_api/test_helpers/content_item_helpers"
+require "json"
+
+module PublishingPlatformApi
+  module TestHelpers
+    module ContentStore
+      include ContentItemHelpers
+
+      def content_store_endpoint(draft: false)
+        draft ? PublishingPlatformLocation.find("draft-content-store") : PublishingPlatformLocation.find("content-store")
+      end
+
+      # Stubs a content item in the content store.
+      # The following options can be passed in:
+      #
+      #   :max_age  will set the max-age of the Cache-Control header in the response. Defaults to 900
+      #   :private  if true, the Cache-Control header will include the "private" directive. By default it
+      #             will include "public"
+      #   :draft    will point to the draft content store if set to true
+      def stub_content_store_has_item(base_path, body = content_item_for_base_path(base_path), options = {})
+        max_age = options.fetch(:max_age, 900)
+        visibility = options[:private] ? "private" : "public"
+        body = body.to_json unless body.is_a?(String)
+
+        endpoint = content_store_endpoint(draft: options[:draft])
+        stub_request(:get, "#{endpoint}/content#{base_path}").to_return(
+          status: 200,
+          body:,
+          headers: {
+            cache_control: "#{visibility}, max-age=#{max_age}",
+            date: Time.now.httpdate,
+          },
+        )
+      end
+
+      def stub_content_store_does_not_have_item(base_path, options = {})
+        endpoint = content_store_endpoint(draft: options[:draft])
+        stub_request(:get, "#{endpoint}/content#{base_path}").to_return(status: 404, headers: {})
+      end
+
+      # Content store has gone item
+      #
+      # Stubs a content item in the content store to respond with 410 HTTP Status Code and response body with 'format' set to 'gone'.
+      #
+      # @param base_path [String]
+      # @param body [Hash]
+      # @param options [Hash]
+      # @option options [String] draft Will point to the draft content store if set to true
+      #
+      # @example
+      #
+      #   stub_content_store.stub_content_store_has_gone_item('/sample-slug')
+      #
+      #   # Will return HTTP Status Code 410 and the following response body:
+      #   {
+      #     "title" => nil,
+      #     "description" => nil,
+      #     "format" => "gone",
+      #     "schema_name" => "gone",
+      #     "public_updated_at" => nil,
+      #     "base_path" => "/sample-slug",
+      #     "withdrawn_notice" => {},
+      #     "details" => {}
+      #   }
+      def stub_content_store_has_gone_item(base_path, body = gone_content_item_for_base_path(base_path), options = {})
+        body = body.to_json unless body.is_a?(String)
+        endpoint = content_store_endpoint(draft: options[:draft])
+        stub_request(:get, "#{endpoint}/content#{base_path}").to_return(
+          status: 410,
+          body:,
+          headers: {},
+        )
+      end
+
+      def stub_content_store_isnt_available
+        stub_request(:any, /#{content_store_endpoint}\/.*/).to_return(status: 503)
+      end
+
+      def content_item_for_base_path(base_path)
+        super.merge("base_path" => base_path)
+      end
+    end
+  end
+end
